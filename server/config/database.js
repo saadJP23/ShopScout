25848e296e7c4b528349425a9ce786f03de18591
saadsyed <saadirfan803@gmail.com>
2025-04-11 19:02:52 +0900

    Switch to DATABASE_URL for Railway + local

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,18 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('shopscout', 'root', 'SaadPakistan2013!', {
-  host: 'localhost',
-  port: 3306, // default MySQL port
+// const sequelize = new Sequelize('shopscout', 'root', 'SaadPakistan2013!', {
+//   host: 'localhost',
+//   port: 3306, // default MySQL port
+//   dialect: 'mysql',
+//   logging: false,
+// });
+
+// module.exports = sequelize;
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   logging: false,
 });
