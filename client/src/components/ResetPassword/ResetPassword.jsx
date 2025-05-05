ed832434abbc35c36344ad8cb18592177dd1fe0f
saadsyed <saadirfan803@gmail.com>
2025-05-05 19:57:18 +0900

    updated reset password for cross platform message

--- client/src/components/ResetPassword/ResetPassword.jsx
+++ client/src/components/ResetPassword/ResetPassword.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./ResetPassword.css";
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
   const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
   const { token } = useParams();
-  const navigate = useNavigate();
   const BASE_URL = "https://api.shopscout.org";
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const res = await axios.post(
-        `${BASE_URL}/user/reset_password`,
-        {
-          token, // ✅ Send token in the body, not URL
-          newPassword: password,
-        }
-      );
+      const res = await axios.post(`${BASE_URL}/user/reset_password`, {
+        token,
+        newPassword: password,
+      });
       setMsg(res.data.msg);
     } catch (err) {
       setMsg(err.response?.data?.msg || "Reset failed");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="reset-page">
+      {loading && (
+        <div className="spinner-overlay">
+          <div className="spinner"></div>
+        </div>
+      )}
       <div className="reset-container">
         <h2>Reset Password</h2>
         <form onSubmit={handleSubmit}>
@@ -40,7 +43,9 @@ const ResetPassword = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button type="submit">Reset</button>
+          <button type="submit" disabled={loading}>
+            Reset
+          </button>
         </form>
         {msg && (
           <p
